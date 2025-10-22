3ce0c7467a9a96efa5d9353ea883af264ad5d41c
PetrPraveky <p1p1kucera@seznam.cz>
2025-10-22 10:56:23 +0200

    Instruction B

--- TargetHandler.py
+++ TargetHandler.py
@@ -11,11 +11,28 @@ class TargetHandler():
 
         By default the final instruction is 2cm above base
         """
-        instructions = np.array([])
+        instructions = []
         
+        height = 0.2 # 20cm height
+
+        instructions[0] = [0, 0, -(height - ground_offset + self.handle_offset)]
+
+        return np.array(instructions)
+    
+    def get_instructions_B(self, ground_offset=0.02):
+        """
+        Returns instructions for labyrint type A.
+
+        By default the final instruction is 2cm above base
+        """
+        instructions = []
+
         height = 0.2
 
-        instructions[0] = -(height - ground_offset + self.handle_offset)
+        instructions[0] = [0.07, 0, 0]
+        instructions[1] = [0, 0, -(self.handle_offset + 0.05)]
+        instructions[2] = [-0.07, 0, -0.07]
+        instructions[3] = [0, 0, -0.08 + ground_offset]
 
-        return instructions
+        return np.array(instructions)
 
